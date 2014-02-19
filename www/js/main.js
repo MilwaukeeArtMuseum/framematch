5fb996de5f7b11c1964e13fd735659e435df70b9
Dustin DuPree <dujodu@gmail.com>
2014-02-19 17:11:41 -0600

    refactoring
    moved ArtFrame class outside of main.js

--- www/js/main.js
+++ www/js/main.js
@@ -36,61 +36,6 @@ var loadManifest = [];
 }());
 
 
-(function() {
-
-	var ArtFrame = function(bmp) {
-		
-		var t = this;
-
-		t.bmp = bmp;
-		
-		t.frameContainer = new createjs.Container();
-		t.frameContainer.addChild(t.bmp);
-		
-		t.frameTop = new createjs.Container();
-		
-		t.frameContainer.addChild(t.frameTop);
-		t.frameBot = new createjs.Container();
-		
-		t.frameContainer.addChild(t.frameBot);
-		t.artContainer = new createjs.Container();
-		
-		t.frameContainer.addChild(t.artContainer);
-
-		t.originalX = 0;
-		t.originalY = 0;
-		t.artScale = 1;
-
-	}
-
-	ArtFrame.prototype.setPoint = function(pt) {
-		this.artPoint = pt;
-		this.artContainer.x = this.artPoint.x;
-		this.artContainer.y = this.artPoint.y;
-	};
-	ArtFrame.prototype.setScale = function(scale) {
-		this.artScale = scale;
-	}
-	ArtFrame.prototype.setXY = function(pt) {
-		
-		this.frameContainer.setTransform(pt.x,pt.y);
-		this.originalX = pt.x;
-		this.originalY = pt.y;
-	};
-	ArtFrame.prototype.setFrameHeight = function(height) {
-		this.frameContainer.scaleY = height;
-	}
-
-	ArtFrame.FRAME_TYPES = {KING_LOUIS:"frame_kinglouis",
-							FRAME_NARROW:"frame_narrow",
-							FRAME_REVERSE:"frame_reverse",
-							CASSETTA:"frame_cassetta",
-						   }
-
-	window.ArtFrame = ArtFrame;
-
-}());
-
 function init() {
 
 	if (window.top != window) {
